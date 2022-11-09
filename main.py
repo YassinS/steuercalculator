ea0b047573e8c621cacb18082c5a6d0dfc1456f3
YassinS <we.yassin@googlemail.com>
2022-11-09 08:32:00 +0100

    new file:   __pycache__/interface.cpython-310.pyc 	modified:   main.py 	modified:   templates/index.html 	modified:   templates/result.html

--- main.py
+++ main.py
@@ -7,27 +7,34 @@ app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
 
 @app.route('/',methods=('GET','POST'))
 def index():
-    values = 0
     if request.method == 'POST':
-        gehalt = request.form['gehalt']
-        steuersatz = request.form['steuersatz']
-
+        gehalt = request.form['IB_Brutto']
+        steuerklasse = request.form['IB_Steuerklasse']
+        try:
+            kirche = request.form.get('Kirche') 
+        except Exception:
+            kirche = False
         if not gehalt:
             flash('Gehalt wird benötigt')
-        elif not steuersatz:
-            flash('Steuersatz wird benötigt')
+        elif not steuerklasse:
+            flash('Steuerklasse wird benötigt')
         else:
-            #values = {"gehalt":gehalt,"steuersatz":steuersatz}
-            calculated_value_tax = float(gehalt) * (float(steuersatz)/100)
+            calculated_value_tax = float(gehalt) * (float(steuerklasse)/100)
             calculated_value_tax = round(calculated_value_tax,2)
-            return render_template("result.html",tax=calculated_value_tax)
+            if kirche:
+                kirchensteuer = round(float(gehalt) * 0.09,2)
+                values = {"gehalt":gehalt,"steuerklasse":steuerklasse,"kirche":kirche,"kirchensteuer":kirchensteuer}
+            else:
+                
+                values = {"gehalt":gehalt,"steuerklasse":steuerklasse,"kirche":"Nein"}
+            return render_template("result.html",tax=calculated_value_tax,values=values)
     if request.method=="GET":
         return render_template("index.html")
 
 
 @app.route('/result')
 def result():
-    return render_template("result.html",values=values)
+    return render_template("result.html")
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1800,debug=True)
 
