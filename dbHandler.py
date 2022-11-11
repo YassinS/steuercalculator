e52982d04c506b9d9d244243398cc47a01619da5
Luca <mikeska.luca@googlemail.com>
2022-11-11 12:17:42 +0100

    modified:   dbHandler.py

--- dbHandler.py
+++ dbHandler.py
@@ -1,18 +1,24 @@
 import csv
 import sqlite3
 
+
+with open('./Lohnsteuertabelle.sql', 'r') as sqlFile:       #+ SQL Datei wird eingelesen und als "Script"-String
+        sqlScript = sqlFile.read()                          #+ abgespeichert
+
 try:
-    con = sqlite3.connect("Lohnesteuertabelle.sql")         #+ Versucht hier in diesen Try&Catch Block eine
-    print("Connection Successful")                          #+ Verbindung zur Datenbank herzustellen
+    con = sqlite3.connect("localDB.dat")                    #+ Versucht hier in diesen Try&Catch Block eine
+    print("Connection Successful")                          #+ Datenbank zu erstellen und dann sich zu verbinden
 except sqlite3.Error as error:
     print("Connection failed")
 
     cur = con.cursor()                                      #+ Ein Cursor Element, welches die Datenbank
                                                             #+ Befehle weiter an die Datenbank leitet
 
-    readDB = cur.fetchall()                                 #+ Liest einfach alles aus der Datenbank aus
+    cur.executescript(sqlScript)                            #+ Datenbank wird mithilfe der .sql Datei gefüllt
 
-    for i in readDB:                                        
+    #+readTable = cur.execute("SELECT * FROM Lohnsteuertabelle")                              #+ Alle Daten werden ausgelesen
+    readTable = cur.fetchall()
+    for i in readTable:
         print(i[None])
 
     cur.close()                                             #+ Cursor Element wird beendet
